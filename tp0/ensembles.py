a5cd0c24afdf84296b8b4dccc9bb16c400fe2603
pallian55 <lilianhannequin@gmail.com>
2026-09-15 15:47:21 +0200

    question 2: ajout des fonctions modifiant des ensembles de robots

--- tp0/ensembles.py
+++ tp0/ensembles.py
@@ -41,3 +41,28 @@ exploration_seule = robots_exploration_seulement(robots_exploration, robots_tran
 assert double_mission == {"R5", "R7"}
 assert toutes_missions == {"R2", "R3", "R5", "R7", "R9"}
 assert exploration_seule == {"R2"}
+
+def ajouter_robot_mission(ensemble, nom) :
+    """Crée un nouvel avec le nouveau robot dedans sans modifier celui passé en argument"""
+    ret = set()
+    for i in ensemble :
+        ret.add(i)
+    ret.add(nom)
+    return ret
+
+def retirer_robot_mission(ensemble, nom) :
+    """
+    Crée un nouvel ensemble sans le robot à retirer et sans modifier l'ensemble passé en argument
+    """
+    ret = set()
+    for i in ensemble :
+        if i != nom :
+            ret.add(i)
+    return ret
+
+ajout = ajouter_robot_mission(robots_exploration, "R8")
+retrait = retirer_robot_mission(robots_transport, "R9")
+assert ajout == {"R2", "R5", "R7", "R8"}
+assert retrait == {"R3", "R5", "R7"}
+# L’ensemble d’origine ne doit pas avoir été modifié
+assert robots_transport == {"R5", "R9", "R7", "R3"}

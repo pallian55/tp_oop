a96137c922bd0d4e8e988df3ea989fc7b6ba36e1
pallian55 <lilianhannequin@gmail.com>
2026-09-22 16:13:30 +0200

    Question 2 : Ajout de méthodes d'ajouts et d'affichage a la classe Village

--- tp2/village.py
+++ tp2/village.py
@@ -1,6 +1,7 @@
 """Implementation de la classe Village"""
 from habitant_decorateur import Habitant
 
+
 class Village:
     """Classe représentant un village composé d'habitant"""
     def __init__(self, nom):
@@ -10,3 +11,26 @@ class Village:
     def get_habitants(self):
         """Retourne la liste des habitants du village"""
         return self.__habitants
+
+    def ajouter_habitant_composition(self, nom, age, adresse, animaux = None):
+        """Crée un nouvel objet Habitant et l'ajoute à la liste"""
+        self.get_habitants().append(Habitant(nom, age, adresse, animaux))
+
+    def ajouter_habitant_agregation(self, habitant):
+        """Ajoute à la liste un objet habitant qui existe deja"""
+        self.get_habitants().append(habitant)
+
+    def afficher_habitants(self):
+        """Affiche tous les habitants du village"""
+        for i in self.get_habitants():
+            i.affichage_adresse()
+
+pytown = Village("PyTown")
+pytown.ajouter_habitant_composition("Aldric", 25, "Rue A", {"vaches": 3})
+elise = Habitant("Elise", 28, "Rue B", {"poules": 10})
+pytown.ajouter_habitant_agregation(elise)
+autre_village = Village("VillageVoisin")
+autre_village.ajouter_habitant_agregation(elise)
+# meme habitant dans 2 villages
+assert len(pytown.get_habitants()) == 2
+assert elise in autre_village.get_habitants()

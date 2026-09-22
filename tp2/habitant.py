d8bcec243a77bb5838248b93bd83b2b2a656ca8e
pallian55 <lilianhannequin@gmail.com>
2026-09-22 14:55:59 +0200

    Question 2 : ajout des méthodes affichage_adresse et compte_animal

--- tp2/habitant.py
+++ tp2/habitant.py
@@ -6,3 +6,20 @@ class Habitant :
         self.age = age
         self.adresse = adresse
         self.animaux = animaux
+
+    def affichage_adresse(self):
+        """Affiche le nom et l'adresse de l'habitant"""
+        print(self.nom + " habite a " + self.adresse)
+
+    def compte_animal(self, animal):
+        """Retourne le nombre d'animaux de type animal que posséde l'habitant"""
+        if animal in self.animaux:
+            return self.animaux[animal]
+        else :
+            return 0
+
+h1 = Habitant("Aldric", 25, "Rue A", {"vaches": 3})
+assert h1.nom == "Aldric"
+assert h1.compte_animal("vaches") == 3
+assert h1.compte_animal("moutons") == 0
+h1.affichage_adresse() # affiche "Aldric habite a Rue A"
